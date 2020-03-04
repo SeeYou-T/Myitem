71d7d7ed173dadbb62960c747545a423be156731
谭云锴 <2472746777@qq.com>
2020-03-04 23:50:50 +0800

    详情页

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,7 @@ const serverHandler = () => {
              .pipe(webserver({ // 需要一些配置项
                host: 'www.index.com', // 域名, 这个域名可以自定义
                port: 8080, // 端口号, 0 ~ 65535, 尽量不适用 0 ~ 1023
-               open: './pages/index.html', // 你默认打开的首页, 从 dist 下面的目录开始书写
+               open: './pages/details.html', // 你默认打开的首页, 从 dist 下面的目录开始书写
                livereload: true, // 自动刷新浏览器 - 热重启
                // 所有的代理配置都在 proxies 里面
                proxies: [
@@ -116,7 +116,7 @@ const watchHandler = () => {
   gulp.watch('./src/pages/*.html', htmlHandler)
   gulp.watch('./src/lib/**', libHandler)
   gulp.watch('./src/images/**', imgHandler)
-  gulp.watch('./src/sass/*.scss', sass)
+  gulp.watch('./src/sass/*.scss', sassHandler)
 }
 
 
